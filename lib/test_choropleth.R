8cc6d0a00cfbcbd8d7923dcfb6c862ded6b8b052
He Zhu <hz2429@columbia.edu>
2017-02-19 15:01:35 -0500

    Fix

--- lib/test_choropleth.R
+++ lib/test_choropleth.R
@@ -5,24 +5,28 @@ library(leaflet)
 library(htmlwidgets)
 library(shiny)
 
-setwd('/Users/AaronWang/Desktop/5243-proj2-grp2')
-# data = read.csv("2016_Green_Taxi_Trip_Data.csv", header = T)
-# save(data, file = 'output/2016_green_taxi.RData')
-load('output/2016_green_taxi.RData')
+
+setwd("~/Desktop/Spr2017-proj2-grp2")
+
+#data.sel = read.csv("data/2016_abridged_cleaned_grouped_FpD.csv", header = T)
+#save(data, file = 'output/2016_green_taxi.RData')
+# load('output/2016_green_taxi.RData')
 # myShape1 = readOGR("data/nynta_shapefile/nynta.shp", layer="nynta")
-# save(myShape1, file = 'output/myShape1.RData')
+# save(myShape1, file = 'output/myShape1.RData'
 load('output/myShape1.RData')
 
-data.sel = data[1:200000,]
 
 # calculate the count of pick-ups in each block
 subdat<-spTransform(myShape1, CRS("+init=epsg:4326"))
-dat = data.frame(Longitude = data.sel$Pickup_longitude, Latitude = data.sel$Pickup_latitude)
-coordinates(dat) <- ~ Longitude + Latitude
-proj4string(dat) <- CRS("+proj=longlat")
-dat <- spTransform(dat, proj4string(myShape1))
-r = over(dat, myShape1)
-subdat@data$count = table(r$NTACode)[rank(subdat@data$NTACode)]
+
+
+load('output/count_seperated.RData')
+
+#subdat@data$count = count_result[, input$hour+1, !input$isBussinessDay+1]
+
+# all hours
+subdat@data$count = apply(count_result[, , 1], 1, sum)
+
 subdat_data=subdat@data[,c("NTACode", "NTAName", "count")]
 subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
 
@@ -33,7 +37,15 @@ popup1 = paste0('<strong>Neighborhood: </strong><br>', subdat_data$NTAName,
 mymap = leaflet(subdat) %>%
   setView(lat=40.7128, lng=-74.0059, zoom=10) %>%
   addProviderTiles('CartoDB.Positron') %>%
-  addPolygons(fillColor = ~pal(count), color = 'grey', weight = 1, popup = popup1, fillOpacity = .6)
+  addPolygons(fillColor = ~pal(count), color = 'grey', weight = 1, popup = popup1, fillOpacity = .6) %>%
+
+
+  addLegend(position = 'bottomright', ## choose bottomleft, bottomright, topleft or topright
+          colors = c('#fee0d2','#fb6a4a', '#cb181d', '#a50f15', '#67000d'),
+          labels = c(0,10,100,1000,10000),  ## legend labels (only min and max)
+          opacity = 0.6,      ##transparency again
+          title = "Pick Up<br>Numbers")
+
 print(mymap)
 
 
